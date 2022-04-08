eec2513d812b04d6f6be595842be876e36fa44a7
mousedoc <sangyun3631@gmail.com>
2022-04-08 19:03:30 +0900

    add utils

---
+++ Assets/Editor/Util/EditorPlayPauseShortcut.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using UnityEditor;
+
+public class EditorPlayPauseShortcut
+{
+#if UNITY_EDITOR_WIN
+
+    [MenuItem("Tools/Editor/Play %q", priority = 10)]
+    [SuppressMessage("CodeQuality", "IDE0051:사용되지 않는 private 멤버 제거", Justification = "Called by Unity editor")]
+    private static void SetPlay()
+    {
+        EditorApplication.isPlaying = !EditorApplication.isPlaying;
+    }
+
+    [MenuItem("Tools/Editor/Pause %e", priority = 11)]
+    [SuppressMessage("CodeQuality", "IDE0051:사용되지 않는 private 멤버 제거", Justification = "Called by Unity editor")]
+    private static void SetPause()
+    {
+        EditorApplication.isPaused = !EditorApplication.isPaused;
+    }
+
+#endif
+}

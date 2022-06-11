89203272f4a3bfe236bb04211bb2c37673b0da0c
mousedoc <sangyun3631@gmail.com>
2022-06-11 09:52:10 +0900

    asdf

--- Assets/Script/AA.cs
+++ Assets/Script/AA.cs
@@ -1,18 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AA : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
